c9c658fe7274c721813c2c440f0967dac586656d
CielCXQ <xuqi.20@intl.zju.edu.cn>
2021-05-18 20:05:49 +0800

    Practical6 modified:COVID_19 add the frequency

--- Practical6/COVID_19.py
+++ Practical6/COVID_19.py
@@ -2,11 +2,15 @@
 dict = {'USA':29862124, 'India': 11285561, 'Brazil':11205972, 'Russia':4360823, 'UK': 4234924}
 print(dict)
 #     Make a pie chart for the table
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 #     Pie chat, where the slices will be ordered and plotted counter-clockwis
 labels = 'USA', 'India', 'Brazil', 'Russia', 'UK'
 #     to make the sizes so we need to deal with number
-sizes=[float(dict['USA']), float(dict['India']),float(dict['Brazil']),float(dict['Russia']),float(dict['UK'])] 
+sizes=[float(dict['USA']), float(dict['India']),float(dict['Brazil']),float(dict['Russia']),float(dict['UK'])]
+#make the frequency table 
+total = float(dict['USA']) + float(dict['India']) + float(dict['Brazil']) + float(dict['Russia']) + float(dict['UK'])
+frequency_dict = {'USA':float(dict['USA'])/total , 'India': float(dict['India'])/total, 'Brazil':float(dict['Brazil'])/total, 'Russia':float(dict['Russia'])/total, 'UK': float(dict['UK'])/total}
+print(frequency_dict)
 explode =(0.1,0,0,0,0)
 #     to make a color i like
 colors =['gold', 'yellow', 'green', 'red', 'blue']
--- Practical6/exon_length.py
+++ Practical6/exon_length.py
@@ -1,6 +1,6 @@
 #start to make list for average exon length
 L = []
-gene_lengths=[9410,394141,4442,105338,19149,76779,126550,36296,842,159,81]
+gene_lengths=[9410,394141,4442,105338,19149,76779,126550,36296,842,15981]
 exon_counts=[51,1142,42,216,25,650,32533,57,1,523]
 #average length for every sigle exon 
 for i in range(0,10):
